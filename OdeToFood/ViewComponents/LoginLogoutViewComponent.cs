d3f63cb22facfb85fa74eef38784675b64192c99
Ola Popp <olapopp85@gmail.com>
2017-05-07 09:17:54 +0200

    added some views

---
+++ OdeToFood/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.ViewComponents
+{
+    public class LoginLogoutViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Default");
+        }
+    }
+}
